7a995fd1f610cf860907a6714798998edd594408
Letícia Merss <69372437+leticiamerssENG@users.noreply.github.com>
2022-11-05 16:43:58 -0300

    Update ServicoFreteFedEx.java

--- ADS_DesignPatterns/src/Strategy/frete/ServicoFreteFedEx.java
+++ ADS_DesignPatterns/src/Strategy/frete/ServicoFreteFedEx.java
@@ -3,15 +3,16 @@ package Strategy.frete;
 public class ServicoFreteFedEx implements Fretavel {
 	
 	@Override
-	public double calculaValor(double pesoemKg) {
+	public double calcularValor(double pesoEmKg) {
 		
-		if(pesoemKg < 20.0) {
+		if(pesoEmKg < 20.0) {
 			return 0.0;
 		}
-		if(pesoemKg < 20.0) {
-			return 0.0;
+		if(pesoEmKg <= 39.9) {
+			return 45.0;
 		}
-		continuar...
+		
+		return 75.00;
 	}
 
 }
